bcd77dd46014e475e02ebb38f047b1d758da2fa6
daniil.melnikov.1999 <daniil.melnikov.1999@mail.ru>
2020-05-03 00:00:19 +0700

    Some Changes

---
+++ Assets/Scripts/PyramidBuilder.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PyramidBuilder : MonoBehaviour
+{
+    private List<GameObject> blocks = new List<GameObject>();
+
+    void Start()
+    {
+        int length = 10;
+        for (int x = -length; x <= length; x++)
+        {
+            for (int z = -length; z <= length; z++)
+            {
+                if (Mathf.Abs(x) == length || Mathf.Abs(z) == length)
+                {
+                    MakeCube(new Vector3(x, 0, z));
+                }
+            }
+        }
+    }
+
+    public void MakeCube(Vector3 position)
+    {
+        blocks.Add(Instantiate(gameObject, position, Quaternion.identity));
+    }
+}
